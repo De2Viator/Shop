08340202b76ddc6ba33964e23bb0401716967757
De3Viator <yurasadf2@gmail.com>
2022-04-01 12:54:13 +0300

    add catalog

---
+++ public/js/CatalogComponent.js
@@ -0,0 +1,58 @@
+Vue.component('catalog',{
+    data(){
+        return {
+            catalog: [],
+        }
+    },
+    methods : {
+    },
+    template:`
+    <section class="catalog__container">
+        <ul class="menu">
+        <catalog-item v-for='item of catalog' :catalog-item='item'>
+        </catalog-item>
+        </ul>
+      </section>
+    `,
+    mounted(){
+        this.$root.getJson('/api/catalog').then(data => {
+          for(catalogItem of data) {
+            this.catalog.push(catalogItem);
+          }
+        })
+    }
+})
+
+Vue.component('catalog-item', {
+    props:{
+        catalogItem:Object
+    },
+    methods: {
+      addProduct(item) {
+        this.$root.$refs.cart.addProduct(item)
+      },
+    },
+    template:`
+    <li class="menu__item">
+            <div class="overlay"></div>
+            <img :src="catalogItem.image" :alt="catalogItem.name" class="menu__image" />
+            <div class="buy-button-container">
+              <button class="buy-button buy-btn" @click="addProduct(catalogItem)">
+                <img
+                  src="img/mini-baggage.svg"
+                  alt="baggage"
+                  class="buy-button-image catalog__buy-button-image"
+                />
+                <div class="buy-button-text">Add To Cart</div>
+              </button>
+            </div>
+            <div class="menu__item-wrp">
+              <h4 class="menu__title">{{ catalogItem.product_name }}</h4>
+              <p class="menu__description">
+                {{ catalogItem.description }}
+              </p>
+              <p class="menu__price">\${{ catalogItem.price}}</p>
+            </div>
+    </li>
+    `
+})
--- server/server.js
+++ server/server.js
@@ -17,5 +17,15 @@ app.get('/api/products', (req, res) => {
     })
 });
 
+app.get('/api/catalog', (req,res)=> {
+    fs.readFile('server/db/catalog.json','utf-8', (err,data) => {
+        if(err) {
+            res.sendStatus(404,JSON.stringify({result:0, text: err}))
+        } else {
+            res.send(data)
+        }
+    })
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listen on port ${port}...`));
