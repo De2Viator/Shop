8cb583ebb587b541d655e7f8643172760dc6cbef
De2Viator <troshenkoyura@gmail.com>
2023-07-10 23:20:42 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const cart = require('./cartRouter.js');//обработчик всех запросов корзины
+const handler = require("./handler");
+//обработчик всех запросов корзины
 app.use(express.json());
 app.use('/', express.static('public'));
-app.use('/api/cart', cart);
+
+app.get('/api/cart', (req, res) => {
+    fs.readFile('server/db/userCart.json', 'utf-8', (err, data) => {
+        if(err){
+            res.sendStatus(404, JSON.stringify({result:0, text: err}));
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+app.post('/api/cart', (req, res) => {
+    handler(req, res, 'add', 'server/db/userCart.json');
+});
+
+app.delete('/api/cart', (req, res)=> {
+    handler(req, res, 'delete', 'server/db/userCart.json');
+})
+
+app.put('/api/cart/:id', (req, res) => {
+    handler(req, res, 'change', 'server/db/userCart.json');
+});
+
+app.put('/api/cart/:id/clear', (req, res) => {
+    handler(req, res, 'clear', 'server/db/userCart.json');
+});
 
 app.get('/api/products', (req, res) => {
     console.log(cart)
